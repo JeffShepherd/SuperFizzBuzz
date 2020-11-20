4a0c1dc14849d26cdd2b5cdb1cb5cbe2acc55d9e
Jeff Shepherd <jeff.shep777@gmail.com>
2020-11-20 12:21:15 -0700

    Add second challenge

--- super_fizz_buzz.js
+++ super_fizz_buzz.js
@@ -1,4 +1,4 @@
-
+///start of first challenge
 
 for (i = 0; i < 1001; i++) {
 
@@ -21,3 +21,47 @@ for (i = 0; i < 1001; i++) {
   }
 
 }
+
+///end of first challenge
+
+///start of second
+
+function printSuperFizzBuzz(i) {
+
+  do {
+    if (i % 3 === 0 && i % 5 === 0 && i % 7 === 0) {
+      console.log("SuperFizzBuzz");
+      i++;
+    } else if (i % 3 === 0 && i % 7 === 0) {
+      console.log("SuperFizz");
+      i++;
+    } else if (i % 5 === 0 && i % 7 === 0) {
+      console.log("SuperBuzz");
+      i++;
+    } else if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+      i++;
+    } else if (i % 3 === 0) {
+      console.log("Fizz");
+      i++;
+    } else if (i % 5 === 0) {
+      console.log("Buzz");
+      i++;
+    } else if (i % 7 === 0) {
+      console.log("Super");
+      i++;
+    } else {
+      console.log(i);
+      i++;
+    }
+  }
+  while (i < 1001);
+
+}
+
+//OR
+
+
+
+
+///end of second challenge
